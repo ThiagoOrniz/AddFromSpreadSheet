7cc69ff88e042ab5eb308f0c05b1622a1ea29886
Thiago Orniz Martin <Thiago@Thiagos-MacBook-Pro.local>
2016-11-17 00:07:28 -0200

    remove unneeded files

--- app/controllers/remedies_controller.rb
+++ app/controllers/remedies_controller.rb
@@ -12,42 +12,4 @@ class RemediesController < ApplicationController
     @remedies = Remedy.all 
   end
 
-  def show
-    @remedy = Remedy.find(params[:id])
-  end
-
-  def new
-    @remedy = Remedy.new
-  end
-
-  def edit
-    @remedy = Remedy.find(params[:id])
-  end
-
-  def create
-    @remedy = Remedy.new(remedy_params)
-    
-    if @remedy.save
-      redirect_to remedies_path
-    end
-  end
-
-  def update
-    @remedy = Remedy.find(params[:id])
-
-    if @remedy.update_attributes(remedy_params)
-      redirect_to remedies_path
-    end
-  end
-
-  def destroy
-    @remedy = Remedy.find(params[:id])
-    @remedy.destroy
-    redirect_to remedies_path
-  end
-
-  def remedy_params
-	# params.require(:remedy).permit(:name, :lab_name, :lab_cod,:lab_price,:max_price,:code,:generic,:active_principle)
-  end
-
 end
